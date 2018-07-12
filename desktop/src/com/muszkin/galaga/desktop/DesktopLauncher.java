b2f8bc4cfd3936587c3142e056d962792451ce00
muszkin <muszkin@gmail.com>
2018-07-12 21:15:23 +0200

    init

---
+++ desktop/src/com/muszkin/galaga/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.muszkin.galaga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.muszkin.galaga.Drop;
+import com.muszkin.galaga.GameScreen;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
+		config.title = "Catch droplet";
+		new LwjglApplication(new Drop(), config);
+	}
+}
